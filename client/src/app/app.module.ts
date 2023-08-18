0e4d4db6b7242089feb1972317737dd5fa4a6eb8
Gideon <tranminhquan.dtp1b@gmail.com>
2023-08-18 10:05:36 +0700

    update UI

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,22 +6,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ErrorComponent } from './components/error/error.component';
+
+
+import {HttpClientModule} from '@angular/common/http'
+
+
+//Taiga UI
 import { SharedModule } from './shared/shared/shared.module';
+import { LoadingComponent } from './components/loading/loading.component';
+import { WarningComponent } from './components/warning/warning.component';
+import { ErrorComponent } from './components/error/error.component';
+import { SecondNavbarComponent } from './components/second-navbar/second-navbar.component';
+import { SubNavbarMainComponent } from './components/sub-navbar-main/sub-navbar-main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoadingComponent,
+    WarningComponent,
     ErrorComponent,
+    SecondNavbarComponent,
+    SubNavbarMainComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
       BrowserAnimationsModule,
       TuiRootModule,
       TuiDialogModule,
       TuiAlertModule,
-      SharedModule
+      SharedModule,
 ],
   providers: [{provide: TUI_SANITIZER, useClass: NgDompurifySanitizer}],
   bootstrap: [AppComponent]
