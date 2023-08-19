bae379e241d85616d568e8f4de247062769ce103
Bui Duc Dang Khoa <bddkhoa@gmail.com>
2023-08-19 16:21:43 +0700

    pull

--- client/src/app/pages/admin-user/user.module.ts
+++ client/src/app/pages/admin-user/user.module.ts
@@ -3,14 +3,10 @@ import { CommonModule } from '@angular/common';
 import { UserRoutingModule } from './user-routing.module';
 import { UserComponent } from './user.component';
 import { SharedModule  } from 'src/app/shared/shared/shared.module';
-import { UserInfoComponent } from './components/user-info/user-info.component';
-import { UserViewsComponent } from './components/user-views/user-views.component';
 // 
 @NgModule({
   declarations: [
     UserComponent,
-    UserInfoComponent,
-    UserViewsComponent
   ],
   imports: [
     CommonModule,
