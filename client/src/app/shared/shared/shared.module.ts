061353ce42e89f8a44c7a585dea7c5821cb7d579
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2023-08-21 17:53:16 +0700

    pull

--- client/src/app/shared/shared/shared.module.ts
+++ client/src/app/shared/shared/shared.module.ts
@@ -9,6 +9,7 @@ import { CoreModule } from '../core/core.module';
 import { CdkModule } from '../cdk/cdk.module';
 import { AddOnModule } from '../add-on/add-on.module';
 import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
@@ -21,6 +22,7 @@ import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
     CoreModule,
     CdkModule,
     AddOnModule,
+    ReactiveFormsModule,
   ],
   exports: [
     //modules
@@ -28,11 +30,12 @@ import { SidebarComponent } from 'src/app/components/sidebar/sidebar.component';
     CoreModule,
     CdkModule,
     AddOnModule,
-
+    ReactiveFormsModule,
     //components
     FirstNavbarComponent,
     NavbarUserManagementComponent,
     SidebarComponent,
+    
   ],
   providers: [
     tuiSvgOptionsProvider({
