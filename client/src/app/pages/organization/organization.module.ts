56492a0a1051e9c08066e3a5c663b0656c5de770
Duong <lethuyduong9703@gmail.com>
2023-08-25 14:58:41 +0700

    lead vaf organiaton

--- client/src/app/pages/organization/organization.module.ts
+++ client/src/app/pages/organization/organization.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { OrganizationRoutingModule } from './organization-routing.module';
 import { OrganizationComponent } from './organization.component';
-
+import { SharedModule } from 'src/app/shared/shared/shared.module';
 
 @NgModule({
-  declarations: [
-    OrganizationComponent
-  ],
-  imports: [
-    CommonModule,
-    OrganizationRoutingModule
-  ]
+  declarations: [OrganizationComponent],
+  imports: [CommonModule, OrganizationRoutingModule, SharedModule],
 })
-export class OrganizationModule { }
+export class OrganizationModule {}
