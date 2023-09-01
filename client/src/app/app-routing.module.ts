5c4daefb1125cd2db2a172ac66d377e0cf6e7b0a
Vo Van Viet <vovanviet4444@gmail.com>
2023-09-01 12:48:39 +0700

    many a thing

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -46,13 +46,6 @@ const routes: Routes = [
       ),
   },
   {
-    path: 'role-view2',
-    loadChildren: () =>
-      import('./pages/role-view2/role-view2.module').then(
-        (m) => m.RoleView2Module
-      ),
-  },
-  {
     path: 'signup',
     loadChildren: () =>
       import('./pages/signup/signup.module').then((m) => m.SignupModule),
